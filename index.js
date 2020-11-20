d42e131eb00150aff618c4b8a771e3cded13e2c5
Reuven <retzion@gmail.com>
2020-11-20 09:50:41 -0700

    Response header Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
     : ["Access-Control-Request-Headers", "*; SameSite=Lax"]
 
   // set some response headers
-  res.header("Access-Control-Allow-Headers", "*")
+  res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Credentials", "true")
 
   next()
